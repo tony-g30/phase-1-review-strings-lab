87c1ea74701a856caa252c4bc0d4ffb8db4f2132
tony-g30 <tonyaudo@gmail.com>
2024-07-23 00:25:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Tony Audo';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0).toUpperCase() + '!';
